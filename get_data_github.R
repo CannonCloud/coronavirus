b43ec6338ea8c662a32b2234f22df3dc49b18b39
Lukas Juergensmeier <lukas.juergensmeier@tech-academy.io>
2020-02-28 18:30:14 +0100

    new github data source, adjusted plots

---
+++ get_data_github.R
@@ -0,0 +1,108 @@
+library(tidyverse)
+library(readr)
+library(dplyr)
+library(tidyr)
+library(lubridate)
+library(ggplot2)
+
+##########################################################
+# Download Johns Hopkins CSSE Data from GitHub
+##########################################################
+
+links <- c("https://raw.github.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv",
+           "https://raw.github.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv",
+           "https://raw.github.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv"
+           )
+
+deaths <- read_csv(links[1])
+confirmed <- read_csv(links[2])
+recovered <- read_csv(links[3])
+write_csv(deaths, paste("data\\individual\\", Sys.Date(), "_deaths.csv", sep =""))
+write_csv(confirmed, paste("data\\individual\\", Sys.Date(), "_confirmed.csv", sep =""))
+write_csv(recovered, paste("data\\individual\\", Sys.Date(), "_recovered.csv", sep =""))
+
+dl <- list(deaths, confirmed, recovered)
+
+names(dl) <- c("confirmed", "deaths", "recovered")
+
+dl$confirmed %>% 
+  gather("time", "confirmed", -c(1:4)) %>% 
+    mutate(time = mdy(time)) -> df
+
+dl$deaths %>% 
+  gather("time", "deaths", -c(1:4)) %>% 
+  mutate(time = mdy(time)) %>% 
+  select(deaths) %>% 
+  cbind(df) -> df
+
+dl$recovered %>% 
+  gather("time", "recovered", -c(1:4)) %>% 
+  mutate(time = mdy(time)) %>% 
+  select(recovered) %>% 
+  cbind(df) -> df
+
+# data cleaning and preparation
+# full data set
+df %>% 
+  rename(province = `Province/State`,
+         country = `Country/Region`,
+         latitude = Lat,
+         longitude = Long) %>%  
+  mutate(confirmed = ifelse(is.na(confirmed), 0, confirmed)) %>% 
+  mutate(deaths = ifelse(is.na(deaths), 0, deaths)) %>% 
+  mutate(recovered = ifelse(is.na(recovered), 0, recovered)) -> dfc
+
+dfc$netinfected <- dfc$confirmed - dfc$deaths - dfc$recovered  
+
+dfc %>%
+  select(c("province", "country", "latitude", "longitude", "time", "confirmed", "recovered", "deaths", "netinfected")) -> dfc
+
+write_csv(recovered, paste("data\\total\\", Sys.Date(), "_total.csv", sep =""))
+
+
+# generate aggregate data set over all regions per single date
+dfc %>% 
+  #mutate(date = as.Date(date)) %>% 
+  group_by(time) %>% # reduces granularity to days w/o time
+  summarize(confirmed = sum(confirmed),
+            deaths = sum(deaths),
+            recovered = sum(recovered),
+            netinfected = sum(netinfected)
+  ) -> df_single_day
+
+
+##########################################################
+# Download Google Trends
+##########################################################
+
+library(gtrendsR)
+
+keywords <- c("coronavirus", "china", "superbowl", "sanders", "trump")
+country <- c("US")
+keywords <- c("coronavirus", "china", "webasto", "thüringen", "trump")
+country <- c("DE")
+time <- ("2020-01-01 2020-02-28")
+channel <- "web"
+
+trends <- gtrends(keywords, gprop = channel, geo = country, time = time)
+
+#select only interst over time 
+trends$interest_over_time %>% 
+  mutate(hits = replace(hits, hits == "<1", 0)) %>% 
+  mutate(hits = as.integer(hits)) %>% 
+  mutate(date = as.Date(date)) %>% 
+  mutate(time = as.Date(time)) -> time_trend
+
+
+##########################################################
+# Download Stock Market Data
+##########################################################
+
+library(tidyquant)
+
+sp500_names <- tq_index("S&P500")
+
+sp500 <- tq_get(sp500_names$symbol,
+                from = "2020-01-01",
+                to = "2020-02-28",
+                get = "stock.prices")
--- plotting.R
+++ plotting.R
@@ -1,42 +1,49 @@
-source("get_data.R")
+source("get_data_github.R")
 
 #####################################################
 # Coronavirus infected, deaths, recovered
 #####################################################
 
-dfa %>% 
-  ggplot(aes(x = date)) +
+df_single_day %>% 
+  ggplot(aes(x = time)) +
   geom_line(aes(y = confirmed)) +
-  geom_line(aes(y = net_infected), linetype = "dashed") +
+  geom_line(aes(y = netinfected), linetype = "dashed") +
   geom_line(aes(y = -c(deaths)), color = "red") +
   geom_line(aes(y = -c(recovered)), color = "blue") +
   labs(x = 'Date',
-       y = "Number of People",
-       title = "Coronavirus number of infected, deaths, recovered",
-        subtitle = paste("Last Update:", max(dfa$date)))
+       y = "Number of Patients",
+       title = "COVID-19: Global Number of Infected, Net Infected, Deaths, and Recovered Patients",
+       subtitle = paste("Last Update:", max(df_single_day$time)))
 
 # from long to wide
-dfa %>% 
-  pivot_longer(confirmed:net_infected, names_to = "type", values_to = "amount") -> dfaw
+df_single_day %>% 
+  pivot_longer(confirmed:netinfected, names_to = "type", values_to = "amount") -> df_single_day_long
 
-dfaw %>% 
-  ggplot(aes(x = date)) +
+df_single_day_long %>% 
+  ggplot(aes(x = time)) +
   geom_line(aes(y = amount)) +
+  geom_point(aes(y = amount)) +
   facet_wrap(.~ type, scales = "free_y") +
   labs(x = 'Date',
-       y = "Number of People",
-       title = "Coronavirus Statistics",
-       subtitle = paste("Last Update:", max(dfaw$date)))
-  
-## new time series data
+       y = "Number of Patients",
+       title = "COVID-19 Global Statistics",
+       subtitle = paste("Last Update:", max(df_single_day_long$time)))
 
-tslu %>% 
-  ggplot(aes(x = time)) +
-  geom_point(aes(y = confirmed)) +
- # facet_wrap(.~ type, scales = "free_y") +
-  labs(x = 'Date',
-       y = "Number of People",
-       title = "Coronavirus Confimed Cases")
+#####################################################
+# Coronavirus Maps
+#####################################################
+
+library(leaflet)
+leaflet() %>%
+  addTiles() %>%  # Add default OpenStreetMap map tiles
+  addMarkers(lng=174.768, lat=-36.852, popup="The birthplace of R")
+
+leaflet() %>%
+  addTiles() %>%  # Add default OpenStreetMap map tiles
+  addMarkers(dfc[1:7,], lng = dfc[1:7,]$longitude, lat = dfc[1:7,]$latitude) %>% 
+  addPolygons(dfc[1:7,]$country)
+  addPolygons(stroke = FALSE, smoothFactor = 0.2, fillOpacity = 1,
+              color = ~pal(gdp_md_est))
 
 
 #####################################################
@@ -46,6 +53,7 @@ tslu %>%
 time_trend %>% 
   ggplot(aes(x = date, y = hits)) +
   geom_line() +
+  geom_point() +
   facet_wrap(~keyword) +
   xlab("Time") +
   ylab("Relative Interest") +
@@ -62,6 +70,7 @@ sp500 %>%
   filter(symbol %in% symbols) %>% 
   ggplot(aes(x = date, y = adjusted)) +
   geom_line() +
+  geom_point() +
   facet_wrap(~symbol, scales = "free_y") +
   labs(x = 'Date',
        y = "Adjusted Price",
